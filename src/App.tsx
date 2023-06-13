b7f3b343546678a960b31d44f9e8ccfb28dda722
Tom Raviv <tomr@wix.com>
2023-06-13 21:00:20 +0300

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,301 @@
-import logo from './assets/logo.svg';
-import styles from './App.module.scss';
-
 function App() {
     return (
-        <div className={styles.App}>
-            <header className={styles['App-header']}>
-                <img src={logo} className={styles['App-logo']} alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className={styles['App-link']}
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <section className="py-20">
+            <div className="container px-4 mx-auto">
+                <div className="max-w-xl lg:max-w-2xl mx-auto mb-12 text-center">
+                    <span className="text-xs font-semibold text-indigo-500 uppercase">
+                        Lorem ipsum
+                    </span>
+                    <h2 className="mt-2 mb-4 text-3xl leading-tight md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight font-bold font-heading">
+                        Lorem ipsum dolor sit amet consectutar domor at elis
+                    </h2>
+                    <p className="mb-8 text-base leading-relaxed lg:text-xl lg:leading-relaxed text-gray-500">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque massa
+                        nibh, pulvinar vitae aliquet nec, accumsan aliquet orci.
+                    </p>
+                    <a className="mr-6 text-lg pb-2 border-b-4 border-indigo-500" href="#">
+                        Monthly
+                    </a>
+                    <a
+                        className="text-lg pb-2 border-b-4 border-transparent hover:border-indigo-500"
+                        href="#"
+                    >
+                        Yearly
+                    </a>
+                </div>
+                <div className="flex flex-wrap items-center -mx-4 -mb-6 lg:mb-0">
+                    <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
+                        <div className="p-6 md:p-12 bg-gray-50 rounded lg:text-center">
+                            <h3 className="text-2xl font-bold text-indigo-500">Starter</h3>
+                            <span className="inline-block mb-6 text-4xl md:text-6xl font-bold font-heading">
+                                $34,99
+                            </span>
+                            <ul className="mb-6 text-left text-lg">
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p>Complete documentation</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p>Working materials in Figma</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p>100GB cloud storage</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p>500 team members</p>
+                                </li>
+                            </ul>
+                            <a
+                                className="block w-full px-5 py-3 text-sm text-center bg-indigo-500 hover:bg-indigo-600 text-white font-semibold border border-indigo-500 hover:border-indigo-600 rounded transition duration-200"
+                                href="#"
+                            >
+                                Action
+                            </a>
+                        </div>
+                    </div>
+                    <div className="w-full lg:w-1/3 px-4 mb-6 lg:mb-0">
+                        <div className="p-6 md:p-12 bg-indigo-500 rounded lg:text-center">
+                            <h3 className="text-2xl font-bold text-gray-50">Pro</h3>
+                            <span className="inline-block mb-6 text-4xl md:text-6xl font-bold text-white">
+                                $65,99
+                            </span>
+                            <ul className="mb-6 text-left text-lg">
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-100"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p className="text-lg text-white">Complete documentation</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-100"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p className="text-lg text-white">Working materials in Figma</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-100"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p className="text-lg text-white">100GB cloud storage</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-100"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p className="text-lg text-white">500 team members</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-100"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p className="text-lg text-white">Premium support</p>
+                                </li>
+                            </ul>
+                            <a
+                                className="block w-full px-5 py-3 text-sm text-center bg-white hover:bg-indigo-50 text-indigo-500 font-semibold border border-white hover:border-indigo-50 rounded transition duration-200"
+                                href="#"
+                            >
+                                Action
+                            </a>
+                        </div>
+                    </div>
+                    <div className="w-full lg:w-1/3 px-4">
+                        <div className="p-6 md:p-12 bg-gray-50 rounded lg:text-center">
+                            <h3 className="text-2xl font-bold text-indigo-500">Premium</h3>
+                            <span className="inline-block mb-6 text-4xl md:text-6xl font-bold font-heading">
+                                $99,99
+                            </span>
+                            <ul className="mb-6 text-left text-lg">
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p>Complete documentation</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p>Working materials in Figma</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p>100GB cloud storage</p>
+                                </li>
+                                <li className="mb-2 flex items-center">
+                                    <svg
+                                        className="mr-2 w-6 h-6 text-indigo-500"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        ></path>
+                                    </svg>
+                                    <p>500 team members</p>
+                                </li>
+                            </ul>
+                            <a
+                                className="block w-full px-5 py-3 text-sm text-center bg-indigo-500 hover:bg-indigo-600 text-white font-semibold border border-indigo-500 hover:border-indigo-600 rounded transition duration-200"
+                                href="#"
+                            >
+                                Action
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
